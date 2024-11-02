106663c271e25daccdae676275cc5ee72beeb4eb
Dariusz12345 <143253881+Dariusz12345@users.noreply.github.com>
2024-11-02 15:20:36 +0100

    Initial commit

---
+++ parcel-project-template-main (2)/parcel-project-template-main/src (6)/src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intervalId = null;
+
+startButton.addEventListener('click', onStart);
+stopButton.addEventListener('click', onStop);
+
+function onStart() {
+  // Dezaktywuj przycisk "Start"
+  startButton.disabled = true;
+  
+  // Uruchom interwał zmiany koloru
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop() {
+  // Zatrzymaj interwał i aktywuj przycisk "Start"
+  clearInterval(intervalId);
+  startButton.disabled = false;
+}
+
+function getRandomHexColor() {  
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
